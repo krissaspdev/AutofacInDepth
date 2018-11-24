d59ae19bcc2f27db911d06430a450f7e2c376e71
Krzysztof A <krissaspdev@hotmail.com>
2018-11-24 22:32:20 +0100

    Property and method injection

---
+++ PropertyAndMethodInjection/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Autofac;
+using Autofac.Core;
+
+namespace PropertyAndMethodInjection
+{
+    public class Parent
+    {
+        public override string ToString()
+        {
+            return "I'm your father";
+        }
+    }
+
+    public class Child
+    {
+        public string Name { get; set; }
+        public Parent Parent { get; set; }
+
+        public void SetParent(Parent parent)
+        {
+            Parent = parent;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var builder = new ContainerBuilder();
+            builder.RegisterType<Parent>();
+
+            // 1. resolving properties using PropertyAutowired
+            //builder.RegisterType<Child>().PropertiesAutowired();
+
+            //2. setting property manually
+            //builder.RegisterType<Child>()
+            //    .WithProperty("Parent", new Parent());
+
+            //3. setting everything manually using component context
+            //builder.Register(c =>
+            //{
+            //    var child = new Child();
+            //    child.SetParent(c.Resolve<Parent>());
+            //    return child;
+            //});
+
+            //4. using Activating Event Handler
+            builder.RegisterType<Child>()
+                .OnActivated((IActivatedEventArgs<Child> e) =>
+                {
+                    var p = e.Context.Resolve<Parent>(); // resolving
+                    e.Instance.SetParent(p); // setting property
+                });
+
+            var conteiner = builder.Build();
+            var parent = conteiner.Resolve<Child>().Parent;
+
+            Console.WriteLine(parent);
+        }
+    }
+}
