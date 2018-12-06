02a6591e6881d0d18ff5883f452fbaa4af0fedfd
Krzysztof A <krissaspdev@hotmail.com>
2018-12-06 23:15:38 +0100

    Configuring modules, lifetime events, code startup

---
+++ ConfigureModule/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Reflection.Emit;
+using Autofac;
+
+namespace ConfigureModule
+{
+    public interface IVehicle
+    {
+        void Go();
+    }
+
+    public class Truck : IVehicle
+    {
+        private IDriver driver;
+
+        public Truck(IDriver driver)
+        {
+            this.driver = driver ?? throw new ArgumentNullException(nameof(driver));
+        }
+
+        public void Go()
+        {
+            driver.Drive();
+        }
+    }
+
+    public interface IDriver
+    {
+        void Drive();
+    }
+
+    class CrazyDriver : IDriver
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Going too fast and crashing into a tree");
+        }
+    }
+
+    public class SaneDriver : IDriver
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Driving safely to destination");
+        }
+    }
+
+    public class TransportModule: Module
+    {
+        public bool ObeySpeedLimit { get; set; }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            if (ObeySpeedLimit)
+                builder.RegisterType<SaneDriver>().As<IDriver>();
+            else
+                builder.RegisterType<CrazyDriver>().As<IDriver>();
+
+            builder.RegisterType<Truck>().As<IVehicle>();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new TransportModule {ObeySpeedLimit = true});
+
+            using (var container = builder.Build())
+            {
+                container.Resolve<IVehicle>().Go();
+            }
+        }
+    }
+}
---
+++ LifetimeEventsCodeStartup/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Autofac;
+using Autofac.Core;
+
+namespace LifetimeEventsCodeStartup
+{
+    public class Parent
+    {
+        public override string ToString()
+        {
+            return "I am your father";
+        }
+    }
+
+    public class Child
+    {
+        public string Name { get; set; }
+        public Parent Parent { get; set; }
+
+
+        public Child()
+        {
+            Console.WriteLine("Child being created");
+        }
+
+        public void SetParent(Parent parent)
+        {
+            Parent = parent;
+        }
+
+        public override string ToString()
+        {
+            return "Hi there";
+        }
+    }
+
+    public class BadChild : Child
+    {
+        public override string ToString()
+        {
+            return "I hate you";
+        }
+    }
+
+    public class MyClass : IStartable
+    {
+        public MyClass()
+        {
+            Console.WriteLine("MyClass ctor");
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Container being build");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<MyClass>()
+                .AsSelf()
+                .As<IStartable>()
+                .SingleInstance();
+
+            builder.RegisterType<Parent>();
+            builder.RegisterType<Child>()
+                .OnActivating((IActivatingEventArgs<Child> a) =>
+                {
+                    Console.WriteLine("Child activating");
+                    a.Instance.Parent = a.Context.Resolve<Parent>();
+                    //a.ReplaceInstance(new BadChild()); // you can overdrive type with subtype if needed
+                })
+                .OnActivated(a =>
+                {
+                    Console.WriteLine("Child activated");
+                })
+                .OnRelease(a =>
+                {
+                    Console.WriteLine("Child about to be removed");
+                });
+
+            var container = builder.Build();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var child = scope.Resolve<Child>();
+                var parent = child.Parent;
+
+                Console.WriteLine(child);
+                Console.WriteLine(parent);
+            }
+        }
+    }
+}
